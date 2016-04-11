e3df3cdc54fbcb7e2df9b9ec79314e6a1f03ac23
Kai Hendry <hendry@webconverger.com>
2016-04-11 18:27:06 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,9 @@
+Create `.creds.ini` with
+
+	id = 'awsid'
+	secret = 'awssecret'
+	bucket = 'yourbucket'
+
+Don't forget to hide it from being served !
+
+You probably also need to change `s3-ap-southeast-1.amazonaws.com/` to wherever your bucket is located.
---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Upload to static</title>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<meta name="robots" content="noindex">
+<style>
+.inputs { display: flex; flex-direction: column; margin: 3em; align-items: left; justify-content: left;}
+label { padding: 1em; margin: 0.3em; border: thin solid black; border-top-right-radius: 1em; }
+</style>
+
+<?php
+$creds = parse_ini_file("/path/to/.creds.ini");
+$expiry = str_replace('+00:00', '.000Z', date("c", time() + 60*60));
+
+$policy = '{
+"expiration": "' . $expiry . '",
+"conditions": [
+{ "acl": "public-read" },
+["starts-with", "$key", "'.date("Y-m-d").'/"],
+["starts-with", "$Content-Type", ""],
+{ "bucket": "' . $creds["bucket"] . '" }
+]
+}';
+
+$policy_b64 = base64_encode($policy);
+$signature = base64_encode(hash_hmac('sha1', $policy_b64, $creds["secret"], true));
+?>
+
+
+<script>
+function fileSelected(f) {
+	var file = f.files[0];
+	if (file) {
+		var key = new Date().toISOString().slice(0, 10) + '/' + file.name;
+		var fileSize = 0;
+		if (file.size > 1024 * 1024)
+			fileSize = (Math.round(file.size * 100 / (1024 * 1024)) / 100).toString() + 'MB';
+		else
+			fileSize = (Math.round(file.size * 100 / 1024) / 100).toString() + 'KB';
+
+		document.getElementById('fileName').innerHTML = '<a href=http://<?=$creds["bucket"]?>/' + key + '>Name: ' + file.name + '</a>';
+		document.getElementById('fileSize').innerHTML = 'Size: ' + fileSize;
+		document.getElementById('fileType').innerHTML = 'Type: ' + file.type;
+	}
+
+	var fd = new FormData();
+
+	fd.append('AWSAccessKeyId', '<?=$creds["id"]?>');
+	fd.append('policy', '<?=$policy_b64?>')
+	fd.append('signature','<?=$signature?>');
+
+
+	fd.append('key', key);
+	fd.append('acl', 'public-read');
+	fd.append('Content-Type', file.type);
+	fd.append("file", f.files[0]);
+	
+	var xhr = new XMLHttpRequest();
+	xhr.upload.addEventListener("progress", uploadProgress, false);
+	xhr.addEventListener("load", uploadComplete, false);
+	xhr.addEventListener("error", uploadFailed, false);
+	xhr.addEventListener("abort", uploadCanceled, false);
+	xhr.open("POST", "https://s3-ap-southeast-1.amazonaws.com/<?=$creds["bucket"]?>");
+	xhr.send(fd);
+}
+
+function uploadProgress(evt) {
+	if (evt.lengthComputable) {
+		var percentComplete = Math.round(evt.loaded * 100 / evt.total);
+		document.getElementById('progressNumber').innerHTML = percentComplete.toString() + '%';
+	}
+	else {
+		document.getElementById('progressNumber').innerHTML = 'unable to compute';
+	}
+}
+
+function uploadComplete(evt) {
+	/* This event is raised when the server send back a response */
+	document.getElementById('response').innerHTML = evt.target.responseText;
+}
+
+function uploadFailed(evt) {
+	alert("There was an error attempting to upload the file.");
+}
+
+function uploadCanceled(evt) {
+	alert("The upload has been canceled by the user or the browser dropped the connection.");
+}
+
+</script>
+</head>
+<body>
+
+<!--<pre>
+<?=$policy?>
+</pre>-->
+
+<p id=response></p>
+
+<div id="fileName"></div>
+<div id="fileSize"></div>
+<div id="fileType"></div>
+<div id="progressNumber"></div>
+
+<div class=inputs>
+<label>Upload file <input type=file onchange="fileSelected(this);" name=upload></label>
+</div>
+
+</body>
+</html>
